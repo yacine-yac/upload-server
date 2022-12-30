d35753d12c8f56c630e5be1b0e51453e09ba2a70
yacine <lentente1958@gmail.com>
2022-12-30 23:39:43 +0100

    define upload feature

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
+const global=require("dotenv").config();
 const express=require('express');
 const upload=require('express-fileupload'); 
-
-
+const cors=require('cors');
+/** init express server */
 const app=express();
+const configCors={
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+}
+app.use(cors(configCors));
+/** to config cors, allowing access to server ressources  */
+// app.use(function(req,res,next){
+//     res.header("Access-Control-Allow-Origin", "*");
+//      next(); 
+// })
+
+
 app.get("/",(req,res)=>{
-      res.send("Hello world") 
+      res.append("Access-Control-Allow-Origin", "*")
+      res.send
+      res.send("Hello world");
+      res.end();
 })
-  
-app.listen(5000,(e)=>console.log('console.error:',e));
+/** config upload server 
+ * useTempFiles allows to save files in the memory
+ */
+ app.use(upload({useTempFiles : true}));
+let status= false;
+app.post("/upload",async (req,res)=>{
+    //  res.append("Access-Control-Allow-Origin", "*");
+       console.log('=>>>>>>>>>>>>>>>>>',req.files);
+    
+     res.send('toktok');
+      if(req.files){
+        
+        req.files["uploading[]"].forEach(x=>{
+              x.mv('./uploads/'+x.name); 
+            
+        })
+          if(status){
+                res.send(`files are not uploaded correctly`);
+                res.sendStatus(404);
+          }else{
+             res.send(`ok,goood!`);
+             res.sendStatus(200);
+          } 
+     }else{
+         res.send('error 404 baby!');
+         res.sendStatus(404);
+     }
+    
+});
+app.listen(process.env.PORT,(e)=>console.log('console.error:',e));
+
  
  
